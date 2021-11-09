0ac8eb9801e19bcb03931403b89f80d515c77e49
John Korsnes <johnkors@gmail.com>
2021-11-09 16:07:10 +0100

    Fixes hello test (#1)
    * Adds hello
    
    * Specify first version
    
    * Update readme
    
    * Test older version of GitVersion action
    
    * Doh. Use main
    
    * Rem 0.1

--- source/SomeClassLib.Tests/UnitTest1.cs
+++ source/SomeClassLib.Tests/UnitTest1.cs
@@ -7,10 +7,4 @@ public class UnitTest1
     {
         Assert.Equal("Hello, John", Class1.Greet("John"));
     }
-
-    [Fact]
-    public void FailIt()
-    {
-        Assert.Equal("Hello, Dork", Class1.Greet("John"));
-    }
 }
