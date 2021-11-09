90596ae60c29e780a73e85dc17f0a0d18447b4e4
John Korsnes <johnkors@gmail.com>
2021-11-09 15:44:57 +0100

    yolo

--- source/SomeClassLib.Tests/UnitTest1.cs
+++ source/SomeClassLib.Tests/UnitTest1.cs
@@ -7,4 +7,10 @@ public class UnitTest1
     {
         Assert.Equal("Hello, John", Class1.Greet("John"));
     }
+
+    [Fact]
+    public void FailIt()
+    {
+        Assert.Equal("Hello, Dork", Class1.Greet("John"));
+    }
 }
