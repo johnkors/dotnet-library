417fd596627777dc0ea92a3259988db0f61d01bc
John Korsnes <johnkors@gmail.com>
2025-02-16 20:36:05 +0100

    2025 - updates

--- source/SomeClassLib/Class1.cs
+++ source/SomeClassLib/Class1.cs
@@ -4,6 +4,13 @@ namespace SomeClassLib;
 
 public class Class1
 {
+    private readonly Class1 _otherClass;
+
+    public Class1(Class1 otherClass)
+    {
+        _otherClass = otherClass;
+    }
+
     public static string Greet(string name)
     {
         return $"Hello, {PeerClass.Upper(name)}";
